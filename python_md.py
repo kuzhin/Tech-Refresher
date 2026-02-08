56d4a992a73f22449c89624c5a2859cf05880c67
Ильдар Кужин <kuzhinup@gmail.com>
2026-02-08 22:17:47 +0500

    Подготовка к собесу

--- python_md.py
+++ python_md.py
@@ -9,3 +9,22 @@ class child(father):
 
 obj = child(22)
 print("%d %d" % (obj.a, obj.b))
+
+def memoize(func):
+    cache = {}
+
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+
+    return wrapper
+
+@memoize
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(4))
+pass
